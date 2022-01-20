f0182b3f0ea2fe92d4d7889f83d423810d090e43
NGUYỄN TRỌNG ĐẠI <21127587@student.hcmus.edu.vn>
2022-01-20 10:58:06 +0700

    Them ham GotoXY va CenterAlign

--- HuntingSnake/graphic.h
+++ HuntingSnake/graphic.h
@@ -4,6 +4,7 @@
 #include <iostream>
 #include <iomanip>
 #include <fstream>
+#include <string>
 
 //Fix console window to specify height and width
 //Prevent user from maximizing or scrolling console window
@@ -14,3 +15,9 @@ void LoadMap(int lvl);
 
 //Create area displaying all game properties like point, speed, lvl,...
 void CreatePlayMenu();
+
+//Move the cursor to the position on the screen
+void GoToXY(COORD pos);
+
+//Return centered string
+std::string CenterAlign(const std::string& str, const int& width);
