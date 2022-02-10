611132427f1ef51ed1e978a46a26e0a537a1cab5
NGUYỄN TRỌNG ĐẠI <21127587@student.hcmus.edu.vn>
2022-02-10 13:40:33 +0700

    Update

--- HuntingSnake/struct.h
+++ HuntingSnake/struct.h
@@ -3,21 +3,89 @@
 #include <vector>
 #include <Windows.h>
 
-struct GameMap {
-	int lvl; //Level
-	int maxFood; //Maximum amount of food can be collected in this level
+//--------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+
+//Window & buffer properties
+	//Size
+#define WINDOW_WIDTH 120
+#define WINDOW_HEIGHT 30
+	//Window size scale (%)
+#define WINDOW_SCALE 100
+
+//Font properties
+	//Width(X) & Height(Y) of single character 
+#define FONT_SIZE_X 1
+#define FONT_SIZE_Y 2
+	//Width & Height scale (%)
+#define FONT_SCALE 100
+
+//Play area
+#define PA_X 0
+#define PA_Y 0
+#define PA_DX 94
+#define PA_DY 26
+
+//Game UI position
+	//OBJECTIVE
+#define UI_OJECTIVE_X 103
+#define UI_OJECTIVE_Y 1
+	//CURRENT LVL
+#define UI_LVL_X 113
+#define UI_LVL_Y 10
+	//SPEED
+#define UI_SPEED_X 106
+#define UI_SPEED_Y 13
+	//FOOD
+#define UI_FOOD_X 105
+#define UI_FOOD_Y 15
+	//LIFE
+#define UI_LIFE_X 105
+#define UI_LIFE_Y 17
+	//POINT
+#define UI_POINT_X 102
+#define UI_POINT_Y 23
+	//TITLE AND TEXT
+#define TEXT_SUB_PA 16
+
+//Game mechanics
+
+//--------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+
+struct GameLVL {
+	int maxFood = 6; //Maximum amount of food can be collected in this level.
 	std::vector<COORD> wall; //Wall positions.
-	std::vector<COORD> gate; //Gate positions.
 	COORD food; //Food position.
+	int foodToSpeedUp = 5; //Amount of food to increase speed.
+	float extraTimePerFood;
+	//Modes
+	bool timeLimit = 0;
+	bool canSelfTouching = 0;
 };
 
 struct Snake {
 	int food = 0;
 	std::vector<COORD> body; //Snake's body positions.
 	std::vector<char> bodyPattern;
-	int speed = 10;
+	int speed = 8;
 	char direction = 'd';
 	char curDirection = 'd';
 	bool state; //0 = dead, 1 = alive.
+	unsigned int point = 0;
+	int life = 1;
+	int nextPattern = 0; // 0-31
+	std::string pattern = "21127587";
+};
+
+struct Square {
+	short x;
+	short y;
+	short dx;
+	short dy;
+};
+
+struct Title {
+	std::string text[5];
+	char pattern = '²';
+	int color = 7;
 };
 
